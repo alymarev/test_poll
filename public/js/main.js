32f2b5452ffbf232fee40e98b7c926110d1620aa
itrager <itrager@mail.ru>
2015-05-29 11:58:02 +0600

    added new components

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+/**
+ * Created by лымарев on 28.05.2015.
+ */
+console.log('helllo');
+$(document).ready(function () {
+    console.log('helllo');
+    $("#add_in").click(function (event) {
+        event.preventDefault();
+
+    })
+});
+
+function createPopup(){
+
+}
